80a873c51c63988b2f7dc975b3de055c676129e5
Henri Kuehl <henri.k@hotmail.de>
2024-01-24 16:24:11 +0100

    Camp section finished successfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
-import Features from '@/components/Features'
-import GetApp from '@/components/GetApp'
-import Camp from '@/components/Camp'
-import Hero from '@/components/Hero'
-import Guide from '@/components/Guide'
-import { Cambay } from 'next/font/google'
-import Image from 'next/image'
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     // React Fragment used to group elements when you dont need additional classes
     // Saves Code
     <>
-      <Hero/>
-      <Camp/>
-      <Guide/>
-      <Features/>
-      <GetApp/>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
     </>
-  )
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,85 +1,85 @@
-"use client"
-import { NAV_LINKS } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Button from './Button'
-import { useState } from 'react';
-
+"use client";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Button from "./Button";
+import { useState } from "react";
 
 const Navbar = () => {
   const [navigationState, toggleNavigationState] = useState(false);
   return (
-    <nav className={`flexBetween max-container 
-    padding-container relative z-30 py-5
-    ${
-      navigationState ? 'h-full w-full bg-slate-100' : 'hidden'
-    }`}>
-        <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo"
-         width={74} height={29}/>
-        </Link>
-        <ul className='hidden h-full gap-12 lg:flex'>
-          {NAV_LINKS.map((link) =>(
-            <Link href={link.href} key={link.key} className='regular-16
+    <nav
+      className={`flexBetween max-container 
+      padding-container relative z-30 py-5
+    ${navigationState ? "h-full w-full bg-slate-100" : "hidden"}`}
+    >
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16
            text-gray-50 flexCenter cursor-point pb-1.5 transition-all
-            hover:font-bold'>
+            hover:font-bold"
+          >
             {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className={`flex ${navigationState ? "flex" : "hidden"}`}>
+        <ul className="lg:hidden h-full gap-12 ">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16
+            text-gray-50 flexCenter cursor-point pb-1.5 transition-all
+              hover:font-bold"
+            >
+              {link.label}
             </Link>
           ))}
         </ul>
-        <div className={`flex ${
-          navigationState ? 'flex' : 'hidden'
-        }`}
-        >
-          <ul className='lg:hidden h-full gap-12 '>
-            {NAV_LINKS.map((link) =>(
-              <Link href={link.href} key={link.key} className='regular-16
-            text-gray-50 flexCenter cursor-point pb-1.5 transition-all
-              hover:font-bold'>
-              {link.label}
-              </Link>
-            ))}
-          </ul>    
-        </div>        
-        <div className='lg:flexCenter hidden'>
-          <Button
+      </div>
+      <div className="lg:flexCenter hidden">
+        <Button
           type="button"
           title="Login"
           icon="/user.svg"
           variant="btn_dark_green"
-          />
-        </div>
-        {/* HAMBURGER MENU MOBILE */}
-        <div
-        className='md:hidden inline-block cursor-pointer lg:hidden'>
-          <button
-          className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400'
+        />
+      </div>
+      {/* HAMBURGER MENU MOBILE */}
+      <div className="md:hidden inline-block cursor-pointer lg:hidden">
+        <button
+          className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400"
           onClick={() => toggleNavigationState(!navigationState)}
-          > 
+        >
           {navigationState ? (
-            <Image 
-            src="/close2.svg" 
-            alt="close" 
-            width={24} 
-            height={24}
-            className='inline-block cursor-pointer lg:hidden'
+            <Image
+              src="/close2.svg"
+              alt="close"
+              width={24}
+              height={24}
+              className="inline-block cursor-pointer lg:hidden"
             />
-            ) : (
-          <Image
-            src="menu.svg"
-            alt="menu"
-            width={32}
-            height={32}
-            className='inline-block cursor-pointer lg:hidden'
+          ) : (
+            <Image
+              src="menu.svg"
+              alt="menu"
+              width={32}
+              height={32}
+              className="inline-block cursor-pointer lg:hidden"
             />
           )}
-          </button>
-          
-        </div>
-
+        </button>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
